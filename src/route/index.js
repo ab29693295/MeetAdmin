f1bf28a0706256b41482dc0a8ccb58aa064473a8
lina <865726456@qq.com>
2020-08-27 17:33:01 +0800

    master分支 登录页面样式

--- src/route/index.js
+++ src/route/index.js
@@ -4,14 +4,16 @@ import { Route, Switch } from "react-router-dom";
 import AsyncComponent from "../AsyncComponent";
 
 /*路由配置*/
-const Login = AsyncComponent(() => import("../view/Login")); //会议选择房间
+const Login = AsyncComponent(() => import("../view/Login")); //登录
+const Layout = AsyncComponent(() => import("../view/Layout")); //
+
 class RoutesIndex extends Component {
     render() {
         return (
             <div>
                 <Switch>
                     <Route  path="/login" component={Login}/>
-                    <Route  path="/" component={Login} />
+                    <Route  path="/" component={Layout} />
                 </Switch>
             </div>
         );
--- src/view/Login/index.jsx
+++ src/view/Login/index.jsx
@@ -1,9 +1,13 @@
 import React, {Component} from "react";
-
+import {Button, Input, Icon, Form, Checkbox,Spin } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import styles from './css/index.module.css'
 class Login extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            loading:false
+        }
     }
 
     componentDidMount() {
@@ -11,7 +15,56 @@ class Login extends Component {
 
     render() {
         return (
-            <>登录</>
+            <div className={styles.loginWrap}>
+
+                <Form
+                    name="normal_login"
+                    className={styles.loginForm}
+                    initialValues={{ remember: true }}
+
+                >
+                    <Spin
+                        spinning={this.state.loading}
+                        tip='加载中...'
+                    >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: '请输入用户名!' }]}
+                        className={styles.formItem}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" style={{ color: 'rgba(0,0,0,.25)' }}/>} placeholder="用户名/手机号/邮箱" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: '请输入密码！' }]}
+                        className={styles.formItem}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                            type="password"
+                            placeholder="密码"
+                        />
+                    </Form.Item>
+                        <Form.Item >
+                            <div className={styles.formSetting}>
+                                <Checkbox>记住密码</Checkbox>
+                                <a >忘记密码</a>
+                            </div>
+
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                className={styles.formButton}
+                                htmlType='submit'
+                                type='primary'
+                            >
+                                登录
+                            </Button>
+                        </Form.Item>
+                    </Spin>
+                </Form>
+
+            </div>
         )
     }
 }
