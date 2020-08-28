45dd2fd2d68df21f6c8731d28c44ac7e1c9e32f2
lina <865726456@qq.com>
2020-08-28 09:09:34 +0800

    master分支 登录页面完善

--- src/view/Login/index.jsx
+++ src/view/Login/index.jsx
@@ -1,16 +1,31 @@
 import React, {Component} from "react";
-import {Button, Input, Icon, Form, Checkbox,Spin } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import {Button, Input, Icon, Form, Checkbox, Spin} from 'antd'
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
 import styles from './css/index.module.css'
+
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            loading:false
+            loading: false
         }
+        this.submitFinish = this.submitFinish.bind(this)
     }
 
     componentDidMount() {
+    }
+
+    //验证通过
+    submitFinish(value) {
+        console.log(value)
+
+        this.props.history.push({
+            pathname: '/'
+        })
+    }
+
+    handleRemember(value) {
+
     }
 
     render() {
@@ -20,38 +35,41 @@ class Login extends Component {
                 <Form
                     name="normal_login"
                     className={styles.loginForm}
-                    initialValues={{ remember: true }}
-
+                    initialValues={{remember: true}}
+                    onFinish={this.submitFinish}
                 >
                     <Spin
                         spinning={this.state.loading}
                         tip='加载中...'
                     >
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: '请输入用户名!' }]}
-                        className={styles.formItem}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" style={{ color: 'rgba(0,0,0,.25)' }}/>} placeholder="用户名/手机号/邮箱" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: '请输入密码！' }]}
-                        className={styles.formItem}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" style={{ color: 'rgba(0,0,0,.25)' }}/>}
-                            type="password"
-                            placeholder="密码"
-                        />
-                    </Form.Item>
-                        <Form.Item >
-                            <div className={styles.formSetting}>
-                                <Checkbox>记住密码</Checkbox>
-                                <a >忘记密码</a>
-                            </div>
-
+                        <Form.Item
+                            name="username"
+                            rules={[{required: true, message: '请输入用户名!'}]}
+                            className={styles.formItem}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon"
+                                                         style={{color: 'rgba(0,0,0,.25)'}}/>} autoComplete="off"
+                                   placeholder="用户名/手机号/邮箱"/>
                         </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{required: true, message: '请输入密码！'}]}
+                            className={styles.formItem}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon"
+                                                      style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name='remember' valuePropName="checked" noStyle>
+                                <Checkbox>记住密码</Checkbox>
+                            </Form.Item>
+                            <a className={styles.formSetting}>忘记密码</a>
+                        </Form.Item>
+
                         <Form.Item>
                             <Button
                                 className={styles.formButton}
