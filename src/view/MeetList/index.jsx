d764d4e1c3a10cf4c1a5ece620727401470bcb75
lina <865726456@qq.com>
2021-01-28 16:09:33 +0800

    master 分支  会议列表

--- src/view/MeetList/index.jsx
+++ src/view/MeetList/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Modal, Input, Table, Card, Button, notification, Row, Col, Space} from 'antd';
+import {Modal, Input, Table, Card, Button, message, Row, Col, Space} from 'antd';
 import {SearchOutlined, PlusOutlined, CheckSquareOutlined, CloseCircleOutlined} from '@ant-design/icons';
 import styles from './css/index.module.css'
 import {Link} from 'react-router-dom'
@@ -7,111 +7,6 @@ import {formatDateTime} from '../../common/js/tools'
 import axios from '../../axios'
 
 const {Search} = Input;
-
-
-const columns = [
-    {
-        title: '会议主题',
-        dataIndex: 'roomName',
-        width: '20%',
-        align: 'center'
-    },
-    {
-        title: '开始时间',
-        dataIndex: 'startTime',
-        width: '20%',
-        align: 'center',
-        render: (text, record) => {
-            let dateStr = formatDateTime(new Date(record.startTime))
-            return <span>{dateStr}</span>
-        }
-    },
-    {
-        title: '结束时间',
-        dataIndex: 'endTime',
-        width: '20%',
-        align: 'center',
-        render: (text, record) => {
-            let dateStr = formatDateTime(new Date(record.endTime))
-            return <span>{dateStr}</span>
-        }
-    },
-    {
-        title: '是否公开',
-        dataIndex: 'isPublic',
-        align: 'center',
-        render: (text, record) => {
-            if (record.isPublic == 1) {
-                return <span> 公开</span>
-            } else {
-                return <span>封闭</span>
-            }
-
-        }
-    },
-    {
-        title: '锁定状态',
-        dataIndex: 'lockStatus',
-        align: 'center',
-        render: (text, record, index) => {
-
-            if (record.lockStatus == 1) {
-                return <span> 锁定</span>
-            } else {
-                return <span>未锁定</span>
-            }
-
-        }
-    },
-    {
-        title: '操作',
-        dataIndex: 'id',
-        align: 'center',
-        render: (text, record) => {
-            let delTxt = record.lockStatus == 1 ? '解锁' : '锁定';
-            return (
-                <Space size={5}>
-                    <Button size="small" type="primary" className={` ${record.lockStatus == 1?styles.infoBtn:styles.infoBtn1}`}>{delTxt}</Button>
-                    <Button size="small" onClick={() => del(record)} type="primary" danger>删除</Button>
-                    <Button size="small" type="primary" > 查看</Button>
-                </Space>
-                )
-        }
-    }
-]
-
-const del = (data) => {
-    Modal.confirm({
-        title: "信息",
-        okText: "确认",
-        cancelText: "取消",
-        content: (
-            <div>
-                <p>确定要删除{data.roomname}?</p>
-            </div>
-        ),
-        onOk() {
-            // alert(data.id)
-            var params = {rID: data.id};
-            axios.deleteMeetRoom(params).then((res) => {
-                if (res.success) {
-                    notification.success({
-                        message: "删除成功",
-                    });
-                } else {
-                    notification.warning({
-                        message: "删除失败",
-                    });
-                }
-
-            });
-        },
-        onCancel() {
-        },
-    });
-
-};
-
 
 const onSearch = value => console.log(value);
 
@@ -122,39 +17,170 @@ class MeetList extends Component {
             meetData: [],
             selectedRowKeys: [], //选中的行
             loading: false,
+            params:{
+                page:1,
+                pageSize:10,
+                key:''
+            },
+            pageData:{
+                total:0
+            },
         }
         this.SearchMeet = this.SearchMeet.bind(this);
+        this.changPage=this.changPage.bind(this)
+        this.columns = [
+            {
+                title: '会议主题',
+                dataIndex: 'roomName',
+                width: '20%',
+                align: 'center'
+            },
+            {
+                title: '开始时间',
+                dataIndex: 'startTime',
+                width: '20%',
+                align: 'center',
+                render: (text, record) => {
+                    let dateStr = formatDateTime(new Date(record.startTime))
+                    return <span>{dateStr}</span>
+                }
+            },
+            {
+                title: '结束时间',
+                dataIndex: 'endTime',
+                width: '20%',
+                align: 'center',
+                render: (text, record) => {
+                    let dateStr = formatDateTime(new Date(record.endTime))
+                    return <span>{dateStr}</span>
+                }
+            },
+            {
+                title: '是否公开',
+                dataIndex: 'isPublic',
+                align: 'center',
+                render: (text, record) => {
+                    if (record.isPublic == 1) {
+                        return <span> 公开</span>
+                    } else {
+                        return <span>封闭</span>
+                    }
+
+                }
+            },
+            {
+                title: '锁定状态',
+                dataIndex: 'lockStatus',
+                align: 'center',
+                render: (text, record, index) => {
+
+                    if (record.lockStatus == 1) {
+                        return <span> 锁定</span>
+                    } else {
+                        return <span>未锁定</span>
+                    }
+
+                }
+            },
+            {
+                title: '操作',
+                dataIndex: 'id',
+                align: 'center',
+                render: (text, record) => {
+                    let delTxt = record.lockStatus == 1 ? '解锁' : '锁定';
+                    return (
+                        <Space size={5}>
+                            <Button size="small" type="primary" className={` ${record.lockStatus == 1?styles.infoBtn:styles.infoBtn1}`}>{delTxt}</Button>
+                            <Button size="small" data-record={record} onClick={ this.del.bind(this,record) } type="primary" danger>删除</Button>
+                            <Button size="small" type="primary" > 查看</Button>
+                        </Space>
+                    )
+                }
+            }
+        ]
     }
 
     componentDidMount() {
-        let params = {page: 1};
-        this.getMeetData(params);
+        // let params = {page: 1};
+        this.getMeetData();
     }
 
-    getMeetData(params) {
+    getMeetData() {
+        let {params}=this.state
+        this.setState({
+            loading:true
+        })
         axios.getMeetList(params).then((res) => {
-            console.log(res)
-            this.setState({meetData: res.response.data});
+            if(res.success){
+                let response=res.response
+                this.setState({meetData: response.data,pageData:{...this.state.pageData,total:response.dataCount}});
+            }
+            this.setState({
+                loading:false
+            })
         })
     }
+    //删除
+    del (data){
+        let that=this;
+        Modal.confirm({
+            title: `确定要删除${data.roomName}?`,
+            okText: "确认",
+            cancelText: "取消",
+            onOk() {
+                let params = {rID: data.id};
+                axios.deleteMeetRoom(params).then((res) => {
+                    if (res.success) {
+                        message.success(
+                            "删除成功！",
+                        );
+                        //重新获取数据
+                        that.getMeetData({page: 1,pageSize:10})
+                    } else {
+                        message.error("删除失败！");
+                    }
+
+                });
+            },
+            onCancel() {
+            },
+        });
+
+    };
 
     //检索
     SearchMeet(textValue) {
-        let params = {page: 1, key: textValue};
-        this.getMeetData(params);
+        this.setState({
+            params:{...this.state.params,key:textValue,page: 1}
+        },function () {
+            this.getMeetData();
+        })
+
     }
 
     //审核课程
     CheckMeet() {
 
     }
+
     //选择课程
     onSelectChange = selectedRowKeys => {
         console.log('selectedRowKeys changed: ', selectedRowKeys);
         this.setState({ selectedRowKeys });
     };
+
+    //翻页
+    changPage(page){
+        this.setState({
+            params:{...this.state.params,page: page}
+        },function () {
+            this.getMeetData();
+        })
+
+    }
+
     render() {
-        const {  selectedRowKeys } = this.state;
+        const {  selectedRowKeys,pageData,loading } = this.state;
         const rowSelection = {
             selectedRowKeys,
             onChange: this.onSelectChange,
@@ -187,9 +213,10 @@ class MeetList extends Component {
                 <Table bordered
                        rowKey='id'
                        dataSource={this.state.meetData}
-                       columns={columns}
+                       columns={this.columns}
                        rowSelection={rowSelection}
-                       pagination={{position: ['none', 'bottomRight']}}
+                       pagination={{position: ['none', 'bottomRight'],total:pageData.total,onChange:this.changPage}}
+                       loading={loading}
                 > </Table>
             </Card>
         )
