a9b4fb87a912f4649300e0e5698f78c04c510579
lina <865726456@qq.com>
2021-02-04 10:14:16 +0800

    master 分支  新增 图表展示

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,96 @@
+import React, {Component} from "react";
+import * as echarts from 'echarts'
+import { debounce } from "@/utils";
+class LineChart extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            chart:null
+        }
+    }
+    static defaultProps = {
+        height: "350px",
+        styles: {},
+        className: "",
+    };
+    componentDidMount() {
+        debounce(this.initChart.bind(this), 300)();
+        window.addEventListener("resize", () => this.resize());
+    }
+    componentWillUnmount() {
+        this.dispose();
+    }
+    resize() {
+        const chart = this.state.chart;
+        if (chart) {
+            debounce(chart.resize.bind(this), 300)();
+        }
+    }
+    dispose() {
+        if (!this.state.chart) {
+            return;
+        }
+        window.removeEventListener("resize", () => this.resize()); // 移除窗口，变化时重置图表
+        this.setState({ chart: null });
+    }
+
+    setOptions({ xAxisData,series,title,legend } = {}) {
+        this.state.chart.setOption({
+            title: title,
+            backgroundColor: "#fff",
+            legend:legend,
+            xAxis: {
+                data:xAxisData ,
+                boundaryGap: false,
+                axisTick: {
+                    show: false,
+                },
+            },
+            grid: {
+                left: 10,
+                right: 10,
+                bottom: 10,
+                top: 100,
+                containLabel: true,
+            },
+            tooltip: {
+                trigger: "axis",
+                axisPointer: {
+                    type: "cross",
+                },
+                padding: [5, 10],
+            },
+            yAxis: {
+                axisTick: {
+                    show: false,
+                },
+            },
+            series:series
+        });
+    }
+
+    initChart() {
+        if (!this.el) return;
+        this.setState({ chart: echarts.init(this.el) }, () => {
+            this.setOptions(this.props.chartData);
+        });
+    }
+
+    render() {
+        const { className, height, width,styles } = this.props;
+        return (
+            <div
+                id='main'
+                className={className}
+                ref={(el) => (this.el = el)}
+                style={{
+                    ...styles,
+                    height,
+                    width,
+                }}
+            />
+        );
+    }
+}
+
+export default LineChart
--- src/view/Home/index.jsx
+++ src/view/Home/index.jsx
@@ -1,10 +1,63 @@
 import React, {PureComponent} from "react";
-import {Card} from "antd";
-
+import {Card,Row,Col,Avatar} from "antd";
+import {
+    SoundOutlined,
+    ClockCircleOutlined,
+    InfoCircleOutlined,
+    PushpinOutlined
+} from '@ant-design/icons';
+import LineChart from '@/components/Charts/LineChart.jsx'
 class Home extends PureComponent {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            lineChartData:{
+                title:{
+                    left: 'center',
+                    padding: 0,
+                    text:'会议数量图表'
+                },
+                xAxisData:["2021-1-1", "2021-1-2", "2021-1-3", "2021-1-4", "2021-1-5", "2021-1-6", "2021-1-7"],
+                series:[
+                    {
+                    name: "发起会议数量",
+                    itemStyle: {
+                        normal: {
+                            color: "#FF005A",
+                            lineStyle: {
+                                color: "#FF005A",
+                                width: 2,
+                            },
+                        },
+                    },
+                    smooth: true,
+                    type: "line",
+                    data: [1,2,3,4,5,6],
+                    animationDuration: 2800,
+                    animationEasing: "cubicInOut",
+                },{
+                    name: "参与会议数量",
+                    itemStyle: {
+                        normal: {
+                            color: "#3888fa",
+                            lineStyle: {
+                                color: "#3888fa",
+                                width: 2,
+                            },
+                        },
+                    },
+                    smooth: true,
+                    type: "line",
+                    data: [1,10,3,6,5,9],
+                    animationDuration: 2800,
+                    animationEasing: "cubicInOut",
+                }],
+                legend: {
+                    left:'left',
+                    data: ['发起会议数量', '参与会议数量']
+                },
+            }
+        }
     }
 
     componentDidMount() {
@@ -13,7 +66,62 @@ class Home extends PureComponent {
     render() {
         return (
             <Card title="首页"  >
+                <Row gutter={40} align='middle'>
+                    <Col span={6} >
+                        <Card  hoverable>
+                            <Card.Meta
+                                avatar={
+                                    <Avatar icon={<SoundOutlined />} style={{ backgroundColor: '#40c9c6' }} size={64}/>
+                                }
+                                title="我发起的会议"
+                                description="500个"
+                                style={{display:'flex',justifyContent:'space-between'}}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={6}>
+                        <Card  hoverable>
+                            <Card.Meta
+                                avatar={
+                                    <Avatar icon={<PushpinOutlined />} style={{ backgroundColor: '#36a3f7' }} size={64}/>
+                                }
+                                title="我参与的会议"
+                                description="500个"
+                                style={{display:'flex',justifyContent:'space-between'}}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={6}>
+                        <Card  hoverable>
+                            <Card.Meta
+                                avatar={
+                                    <Avatar icon={<ClockCircleOutlined />} style={{ backgroundColor: '#f4516c' }} size={64}/>
+                                }
+                                title="参会总时长"
+                                description="100小时"
+                                style={{display:'flex',justifyContent:'space-between'}}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={6}>
+                        <Card  hoverable>
+                            <Card.Meta
+                                avatar={
+                                    <Avatar icon={<InfoCircleOutlined />} style={{ backgroundColor: '#f6ab40' }} size={64}/>
+                                }
+                                title="我的消息"
+                                description="This is the description"
+                                style={{display:'flex',justifyContent:'space-between'}}
+                            />
+                        </Card>
+                    </Col>
 
+                </Row>
+                <LineChart  chartData={this.state.lineChartData} styles={{
+                    padding: 12,
+                    backgroundColor: "#fff",
+                    margin: "30px 0",
+                }}/>
             </Card>
         )
     }
