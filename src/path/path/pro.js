9ec054c77b8ae525b84d69cd965ffa8ad3da0aeb
LiNa <865726456@qq.com>
2022-04-21 09:46:49 +0800

    develop分支 修改正式域名

--- src/path/path/pro.js
+++ src/path/path/pro.js
@@ -1,5 +1,5 @@
 export default {
-    // "cnkiDomain": "rtc.cnki.net",
-    "cnkiDomain": "cnki.385073012.cn",
-    "cnkiDomain1":"//webrtc.cnki.net"
+    "meetDomain":'https://meetvideo.cnki.net',
+    "tuDomain":"https://webrtc.cnki.net",
+    "cnkiDomain": "https://webrtc.cnki.net",
 };
--- src/path/path/test.js
+++ src/path/path/test.js
@@ -1,6 +1,5 @@
 export default {
     "cnkiDomain": "https://www.zhuimengnanhai.com:8055",
-    "meetDomain":'https://meetvideo.cnki.net',
-    "tuDomain":"https://www.zhuimengnanhai.com:8055",
-    "meetFrontDomain":"https://www.zhuimengnanhai.com:8988"
+    "meetDomain":'https://www.zhuimengnanhai.com:8988',
+    "tuDomain":"https://www.zhuimengnanhai.com:8055"
 };
